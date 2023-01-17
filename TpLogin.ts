b454bdd4e3e2219d3567dedd8098cb680b1a980d
laurence-x <lpemailx@gmail.com>
2023-01-17 11:06:57 +0200

    Create TpLogin.ts

---
+++ TpLogin.ts
@@ -0,0 +1,16 @@
+export type checksT = {
+	Em: React.RefObject<HTMLInputElement>
+	Pw: React.RefObject<HTMLInputElement>
+	ms: React.RefObject<HTMLParagraphElement>
+	iB: React.RefObject<HTMLInputElement>
+	rD: React.RefObject<HTMLDivElement>
+	nvg: (arg: string) => void
+}
+
+export type sendT = {
+	Em: React.RefObject<HTMLInputElement>
+	Pw: React.RefObject<HTMLInputElement>
+	ms: React.RefObject<HTMLParagraphElement>
+	rD: React.RefObject<HTMLDivElement>
+	nvg: (arg: string) => void
+}
